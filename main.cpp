55501a1415f2eff4e73654e331b03730860f0e16
sibjor <sixteen610@gmail.com>
2025-05-08 13:47:19 +0200

    HTML Defined!

---
+++ main.cpp
@@ -0,0 +1,76 @@
+/*
+    Copyright (c) 2025 Sixten Björling
+
+    (https://sibjor.se)
+
+    The MUPUS C++ document might be large, please SEARCH to locate your interests!
+*/
+
+#include <iostream>
+#include <vector>
+#include <filesystem>
+
+class MUPUS
+{
+    struct ForeginSyntax
+    {
+        
+        struct Markup
+        {
+            struct HTML
+            {
+            };
+            struct Markdown
+            {
+
+            };
+        };
+        struct Stylesheet
+        {
+            struct CSS
+            {
+            };
+            
+        };
+        struct CPP
+        {
+            struct C
+            {
+
+            };
+        };
+        struct Python
+        {
+        };
+        struct Java
+        {
+        };
+        struct TypeScript
+        {
+            struct JavaScript
+            {
+            };
+        };
+        struct CSharp
+        {
+        };
+        struct Go
+        {
+        };
+        struct Rust
+        {
+        };
+        struct PHP
+        {
+        };
+        struct Ruby
+        {
+        };
+        struct Swift
+        {
+        };
+        struct Kotlin
+        {
+        };
+    };
+};
